8306db347d5ac15cb35335d0ac8426c5eac10311
Galinaag4 <Galinaag@yandex.ru>
2022-08-10 12:04:42 +0300

    в 3ей задаче не понимаю как вывести только последние 4 года

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+     // Задача 1
+    int s = 0;
+    while(s<=10){System.out.print(s+" ");
+    s ++ ;}
+    System.out.print("\n");
+    int i = 10;
+    for ( i=10; i>0; i--) {System.out.print( i+" ");}
+    // Задача 2
+    System.out.print("\n");
+    for(int p = 5; p<=31; p = p + 7){
+    System.out.println("Сегодня пятница."+(p)+"число. Необходимо подготовить отчет");}
+    // Задача 3
+        for(var k = 0; k <=2021;k = k + 79){System.out.println(k+79);}
+
+
+
+
+    }
+
+}
